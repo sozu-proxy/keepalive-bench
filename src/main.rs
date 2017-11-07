cb30cf7bd87f729bd9dc48c0ada32c97b6e6ab36
Geoffroy Couprie <geo.couprie@gmail.com>
2017-11-07 17:58:50 +0100

    write the results to a CSV file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![feature(conservative_impl_trait)]
 
-extern crate futures;
+extern crate csv;
 extern crate hyper;
+extern crate futures;
 extern crate tokio_core;
-
 extern crate pretty_env_logger;
 
-use std::{env,str};
+use std::{env,str,time};
+use std::fs::File;
 use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
 
 use tokio_core::reactor::Handle;
 use futures::*;
@@ -36,8 +38,14 @@ fn main() {
     let mut core = tokio_core::reactor::Core::new().unwrap();
     let handle = core.handle();
 
-    let work = future::join_all((1..3).map(|id| {
-      let client = HttpClient::new(&handle, id);
+    let mut wtr = csv::Writer::from_path("data.csv").expect("could not open path to data file");
+    wtr.write_record(&["time", "client", "status", "backend", "source"]);
+
+    let shared_wtr = Arc::new(Mutex::new(wtr));
+    let start = time::Instant::now();
+
+    let work = future::join_all((1..10).map(|id| {
+      let client = HttpClient::new(&handle, id, shared_wtr.clone(), start.clone());
 
       let stream = stream::repeat::<_, Error>(id);
       let u = url.clone();
@@ -53,29 +61,47 @@ fn main() {
 struct HttpClient {
   client: Client<HttpConnector>,
   id:     u32,
+  writer: Arc<Mutex<csv::Writer<File>>>,
+  start:  time::Instant,
 }
 
 impl HttpClient {
-  pub fn new(handle: &Handle, id: u32) -> HttpClient {
+  pub fn new(handle: &Handle, id: u32, writer: Arc<Mutex<csv::Writer<File>>>, start: time::Instant) -> HttpClient {
     let client = Client::configure()
         //.no_proto() if you do that there will be no keep alive
         .keep_alive(true)
         // default true: .keep_alive(true)
         .build(&handle);
 
-    HttpClient { client, id }
+    HttpClient { client, id, writer, start }
   }
 
   pub fn call(&self, url: &Uri) -> impl Future<Item = (), Error = hyper::Error> {
     let id: u32 = self.id.clone();
+    let shared_writer = self.writer.clone();
+    let start = self.start.clone();
+
     self.client.get(url.clone()).and_then(move |res| {
+        let duration = start.elapsed();
+        let secs     = duration.as_secs();
+        let nano     = duration.subsec_nanos();
+        let elapsed  = (nano / 1000000) as u64 + (secs * 1000);
+
+        let status_code = res.status().as_u16();
+
         let backend_id: u32 = res.headers().get_raw("Backend-Id").expect("Backend-Id header not found")
                         .one().and_then(|val| str::from_utf8(val).ok()).expect("there should be only one value")
                         .parse().expect("could not parse id");
         let backend_connection_port: u32 = res.headers().get_raw("Source-Port").expect("Backend-Id header not found")
                         .one().and_then(|val| str::from_utf8(val).ok()).expect("there should be only one value")
                         .parse().expect("could not parse id");
-        println!("[{}] {} backend: {} port: {}", id, res.status().as_u16(), backend_id, backend_connection_port);
+
+        println!("[{}] client: {} status: {} backend: {} port: {}", elapsed, id, status_code, backend_id, backend_connection_port);
+
+        if let Ok(mut writer) = shared_writer.try_lock() {
+          writer.write_record(&[format!("{}", elapsed), format!("{}", id), format!("{}", status_code),
+            format!("{}", backend_id), format!("{}", backend_connection_port)]);
+        }
 
         //println!("Headers: \n{}", res.headers());
         //let conn: Option<&header::Connection> = res.headers().get();
